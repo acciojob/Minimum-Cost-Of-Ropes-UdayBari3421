5f9e0c32c862433490705b6551ef45d83a516c4d
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-22 10:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+	let ropes = document.getElementById("rope-lengths");
+	let arr = ropes.split(",");
+	let f = 0;
+	while (arr.length > 1) {
+		arr.sort((a,b) =>{return (a-b)});
+		let value = parseInt (arr.shift());
+		let value1 = parseInt (arr.shift());
+
+		let main = value + value1;
+		f += main;
+
+		arr.push(main);
+	}
+	
+	let result = document.getElementById("result").value;
+	result.innerText = f;
+	return f;
 }  
